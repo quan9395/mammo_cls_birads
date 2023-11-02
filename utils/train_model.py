43db854c82591d9d0accc0c82140c3eb70892208
quan9395 <60518828+quan9395@users.noreply.github.com>
2023-11-02 14:38:43 +0700

    Update train_model.py

--- utils/train_model.py
+++ utils/train_model.py
@@ -106,7 +106,8 @@ def train(model,
   
                 # print(pred_density.shape)
                 # print(label_density)
-                birads_loss = FocalLoss(gamma=4)(pred_birad, label_birads.long())
+                # birads_loss = FocalLoss(gamma=4)(pred_birad, label_birads.long())
+                birads_loss = criterion(pred_birad, label_birads.long())
                 # density_loss = FocalLoss(gamma=4)(pred_density, label_density.long())
                 total_loss = birads_loss
 
